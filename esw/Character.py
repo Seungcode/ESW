ed30f73ce855f83f22bed2b976f6d7a99a033173
Seungcode <ehrehrdltkfk@gmail.com>
2022-11-15 05:52:12 +0900

    메인 캐릭터(화난 토끼)

--- esw/Character.py
+++ esw/Character.py
@@ -1,138 +1,144 @@
 import numpy as np
 import cv2 as cv
 from BackGround import BackGround
+from PIL import Image
 class Character:
-    def __init__(self):
-        self.canvas = np.zeros((40, 40, 3), dtype = np.uint8)
-        self.position = np.array([BackGround.center[0]/2 - 20, BackGround.center[1]/2 - 20, BackGround.center[0]/2 + 20, BackGround.center[1]/2 + 20])
-        self.center = np.array([(self.position[0] + self.position[2]) / 2, (self.position[1] + self.position[3]) / 2])
+    def __init__(self, background):
+        #토끼 (최종)
+        self.canvas = background
+        #self.position = np.array([BackGround.center[0]/2 - 20, BackGround.center[1]/2 - 20, BackGround.center[0]/2 + 20, BackGround.center[1]/2 + 20])
+        #self.center = np.array([(self.position[0] + self.position[2]) / 2, (self.position[1] + self.position[3]) / 2])
         self.state = None
+        x=120
+        y=120
+        black = (0, 0, 0)
         white = (255, 255, 255)
-        pink = (203, 192, 255)
-        red = (0, 0, 255)
+        pink = (255, 192, 203)
+        red = (255, 0, 0)
         #발
-        cv.line(self.canvas, (12, 40), (20, 40), white)
-        cv.line(self.canvas, (25, 40), (33, 40), white)
+        cv.line(self.canvas, (x+12, y+40), (x+20, y+40), black)
+        cv.line(self.canvas, (x+25, y+40), (x+33, y+40), black)
 
-        cv.line(self.canvas, (12, 40), (12, 38), white)
-        cv.line(self.canvas, (33, 40), (33, 38), white)
+        cv.line(self.canvas, (x+12, y+40), (x+12, y+38), black)
+        cv.line(self.canvas, (x+33, y+40), (x+33, y+38), black)
 
-        cv.line(self.canvas, (12, 38), (18, 38), white)
-        cv.line(self.canvas, (33, 38), (27, 38), white)
+        cv.line(self.canvas, (x+12, y+38), (x+18, y+38), black)
+        cv.line(self.canvas, (x+33, y+38), (x+27, y+38), black)
 
-        cv.line(self.canvas, (17, 38), (17, 37), white)
-        cv.line(self.canvas, (28, 37), (28, 38), white)
+        cv.line(self.canvas, (x+17, y+38), (x+17, y+37), black)
+        cv.line(self.canvas, (x+28, y+37), (x+28, y+38), black)
 
-        cv.line(self.canvas, (20, 37), (20, 40), white)
-        cv.line(self.canvas, (25, 37), (25, 40), white)
+        cv.line(self.canvas, (x+20, y+37), (x+20, y+40), black)
+        cv.line(self.canvas, (x+25, y+37), (x+25, y+40), black)
         #발 색칠
-        cv.line(self.canvas, (13, 39), (19, 39), pink)
-        cv.line(self.canvas, (26, 39), (32, 39), pink)
-        cv.rectangle(self.canvas, (19, 38), (20, 37), pink, -1)
-        cv.rectangle(self.canvas, (27, 38), (26, 37), pink, -1)
+        cv.line(self.canvas, (x+13, y+39), (x+19, y+39), pink)
+        cv.line(self.canvas, (x+26, y+39), (x+32, y+39), pink)
+        cv.rectangle(self.canvas, (x+18, y+38), (x+29, y+37), pink, -1)
+        cv.rectangle(self.canvas, (x+27, y+38), (x+26, y+37), pink, -1)
         #몸통
-        cv.line(self.canvas, (15, 36), (34, 36), white)
+        cv.line(self.canvas, (x+15, y+36), (x+30, y+36), black)
 
-        cv.line(self.canvas, (15, 36), (15, 35), white)
-        cv.line(self.canvas, (30, 36), (30, 35), white)
+        cv.line(self.canvas, (x+15, y+36), (x+15, y+35), black)
+        cv.line(self.canvas, (x+30, y+36), (x+30, y+35), black)
 
-        cv.line(self.canvas, (14, 35), (15, 35), white)
-        cv.line(self.canvas, (30, 35), (31, 35), white)
+        cv.line(self.canvas, (x+14, y+35), (x+15,y+35), black)
+        cv.line(self.canvas, (x+30, y+35), (x+31, y+35), black)
 
-        cv.line(self.canvas, (14, 35), (14, 34), white)
-        cv.line(self.canvas, (31, 35), (31, 34), white)
+        cv.line(self.canvas, (x+14, y+35), (x+14, y+34), black)
+        cv.line(self.canvas, (x+31, y+35), (x+31, y+34), black)
 
-        cv.line(self.canvas, (13, 34), (14, 34), white)
-        cv.line(self.canvas, (32, 34), (31, 34), white)
+        cv.line(self.canvas, (x+13, y+34), (x+14, y+34), black)
+        cv.line(self.canvas, (x+32, y+34), (x+31, y+34), black)
 
-        cv.line(self.canvas, (13, 34), (13, 31), white)
-        cv.line(self.canvas, (32, 34), (32, 31), white)
+        cv.line(self.canvas, (x+13, y+34), (x+13, y+31), black)
+        cv.line(self.canvas, (x+32, y+34), (x+32, y+31), black)
 
-        cv.line(self.canvas, (14, 31), (13, 31), white)
-        cv.line(self.canvas, (31, 31), (32, 31), white)
+        cv.line(self.canvas, (x+14, y+31), (x+13, y+31), black)
+        cv.line(self.canvas, (x+31, y+31), (x+32, y+31), black)
 
-        cv.line(self.canvas, (14, 30), (14, 31), white)
-        cv.line(self.canvas, (31, 30), (31, 31), white)
+        cv.line(self.canvas, (x+14, y+30), (x+14, y+31), black)
+        cv.line(self.canvas, (x+31, y+30), (x+31, y+31), black)
 
-        cv.line(self.canvas, (14, 30), (15, 30), white)
-        cv.line(self.canvas, (31, 30), (30, 30), white)
+        cv.line(self.canvas, (x+14, y+30), (x+15, y+30), black)
+        cv.line(self.canvas, (x+31, y+30), (x+30, y+30), black)
 
-        cv.line(self.canvas, (15, 29), (15, 30), white)
-        cv.line(self.canvas, (30, 29), (30, 30), white)
+        cv.line(self.canvas, (x+15, y+29), (x+15, y+30), black)
+        cv.line(self.canvas, (x+30, y+29), (x+30, y+30), black)
 
-        cv.line(self.canvas, (15, 29), (17, 29), white)
-        cv.line(self.canvas, (30, 29), (28, 29), white)
+        cv.line(self.canvas, (x+15, y+29), (x+17, y+29), black)
+        cv.line(self.canvas, (x+30, y+29), (x+28, y+29), black)
 
-        cv.line(self.canvas, (17, 24), (17, 29), white)
-        cv.line(self.canvas, (30, 24), (30, 29), white)
+        cv.line(self.canvas, (x+17, y+24), (x+17, y+29), black)
+        cv.line(self.canvas, (x+28, y+24), (x+28, y+29), black)
         #몸통 색칠 
-        cv.rectangle(self.canvas, (16, 35), (29, 30), pink, -1)
-        cv.rectangle(self.canvas, (18, 29), (27, 24), pink, -1)
-        cv.line(self.canvas, (15, 34), (15, 31), pink)
-        cv.line(self.canvas, (30, 34), (30, 31), pink)
-        cv.line(self.canvas, (14, 33), (14, 32), pink)
-        cv.line(self.canvas, (31, 33), (31, 32), pink)
+        cv.rectangle(self.canvas, (x+16, y+35), (x+29, y+30), pink, -1)
+        cv.rectangle(self.canvas, (x+18, y+29), (x+27, y+24), pink, -1)
+        cv.line(self.canvas, (x+15, y+34), (x+15, y+31), pink)
+        cv.line(self.canvas, (x+30, y+34), (x+30, y+31), pink)
+        cv.line(self.canvas, (x+14, y+33), (x+14, y+32), pink)
+        cv.line(self.canvas, (x+31, y+33), (x+31, y+32), pink)
         #꼬리
-        cv.rectangle(self.canvas, (22, 34), (23, 33), white)
+        cv.rectangle(self.canvas, (x+22, y+34), (x+23, y+33), black)
         #얼굴
-        cv.line(self.canvas, (15, 23), (30, 23), white)
+        cv.line(self.canvas, (x+15, y+23), (x+30, y+23), black)
 
-        cv.line(self.canvas, (15, 23), (15, 22), white)
-        cv.line(self.canvas, (30, 23), (30, 22), white)
+        cv.line(self.canvas, (x+15, y+23), (x+15, y+22), black)
+        cv.line(self.canvas, (x+30, y+23), (x+30, y+22), black)
 
-        cv.line(self.canvas, (15, 22), (14, 22), white)
-        cv.line(self.canvas, (30, 22), (31, 22), white)
+        cv.line(self.canvas, (x+15, y+22), (x+14, y+22), black)
+        cv.line(self.canvas, (x+30, y+22), (x+31, y+22), black)
 
-        cv.line(self.canvas, (14, 21), (14, 22), white)
-        cv.line(self.canvas, (31, 21), (31, 22), white)
+        cv.line(self.canvas, (x+14, y+21), (x+14, y+22), black)
+        cv.line(self.canvas, (x+31, y+21), (x+31, y+22), black)
 
-        cv.line(self.canvas, (14, 21), (13, 21), white)
-        cv.line(self.canvas, (31, 21), (32, 21), white)
+        cv.line(self.canvas, (x+14, y+21), (x+13, y+21), black)
+        cv.line(self.canvas, (x+31, y+21), (x+32, y+21), black)
 
-        cv.line(self.canvas, (13, 15), (13, 21), white)
-        cv.line(self.canvas, (32, 15), (32, 21), white)
+        cv.line(self.canvas, (x+13, y+15), (x+13, y+21), black)
+        cv.line(self.canvas, (x+32, y+15), (x+32, y+21), black)
 
-        cv.line(self.canvas, (13, 15), (14, 15), white)
-        cv.line(self.canvas, (31, 15), (32, 15), white)
+        cv.line(self.canvas, (x+13, y+15), (x+14, y+15), black)
+        cv.line(self.canvas, (x+31, y+15), (x+32, y+15), black)
 
-        cv.line(self.canvas, (14, 14), (14, 15), white)
-        cv.line(self.canvas, (31, 14), (31, 15), white)
+        cv.line(self.canvas, (x+14, y+14), (x+14, y+15), black)
+        cv.line(self.canvas, (x+31, y+14), (x+31, y+15), black)
 
-        cv.line(self.canvas, (14, 14), (16, 14), white)
-        cv.line(self.canvas, (31, 14), (29, 14), white)
+        cv.line(self.canvas, (x+14, y+14), (x+16, y+14), black)
+        cv.line(self.canvas, (x+31, y+14), (x+29, y+14), black)
 
-        cv.line(self.canvas, (16, 7), (16, 14), white)
-        cv.line(self.canvas, (14, 7), (16, 7), white)
+        cv.line(self.canvas, (x+16, y+7), (x+16, y+14), black)
+        cv.line(self.canvas, (x+14, y+7), (x+16, y+7), black)
 
-        cv.line(self.canvas, (13, 5), (13, 6), white)
+        cv.line(self.canvas, (x+13, y+5), (x+13, y+6), black)
 
-        cv.line(self.canvas, (14, 4), (19, 4), white)
+        cv.line(self.canvas, (x+14, y+4), (x+19,y+4), black)
 
-        cv.line(self.canvas, (19, 11), (19, 4), white)
+        cv.line(self.canvas, (x+19, y+11), (x+19, y+4), black)
 
-        cv.line(self.canvas, (19, 11), (26, 11), white)
+        cv.line(self.canvas, (x+19, y+11), (x+26, y+11), black)
 
-        cv.line(self.canvas, (26, 1), (26, 11), white)
+        cv.line(self.canvas, (x+26, y+1), (x+26, y+11), black)
 
-        cv.line(self.canvas, (27, 0), (28, 0), white)
+        cv.line(self.canvas, (x+27, y+0), (x+28, y+0), black)
 
-        cv.line(self.canvas, (29, 1), (29, 14), white)
-        #몸통 색칠
-        cv.rectangle(self.canvas, (16, 22), (29, 15), pink, -1)
-        cv.rectangle(self.canvas, (17, 14), (28, 12), pink, -1)
-        cv.rectangle(self.canvas, (27, 1), (28, 11), pink, -1)
-        cv.rectangle(self.canvas, (17, 13), (18, 5), pink, -1)
-        cv.rectangle(self.canvas, (16, 6), (14, 5), pink, -1)
-        cv.line(self.canvas, (15, 23), (15, 15), pink)
-        cv.line(self.canvas, (30, 23), (30, 15), pink)
-        cv.line(self.canvas, (14, 22), (14, 16), pink)
-        cv.line(self.canvas, (31, 22), (31, 16), pink)
+        cv.line(self.canvas, (x+29, y+1), (x+29, y+14), black)
+        #얼굴 색칠
+        cv.rectangle(self.canvas, (x+16, y+22), (x+29, y+15), pink, -1)
+        cv.rectangle(self.canvas, (x+17, y+14), (x+28, y+12), pink, -1)
+        cv.rectangle(self.canvas, (x+27, y+1), (x+28, y+11), pink, -1)
+        cv.rectangle(self.canvas, (x+17, y+13), (x+18, y+5), pink, -1)
+        cv.rectangle(self.canvas, (x+16, y+6), (x+14, y+5), pink, -1)
+        cv.line(self.canvas, (x+15, y+22), (x+15, y+15), pink)
+        cv.line(self.canvas, (x+30, y+22), (x+30, y+15), pink)
+        cv.line(self.canvas, (x+14, y+21), (x+14, y+16), pink)
+        cv.line(self.canvas, (x+31, y+21), (x+31, y+16), pink)
         #꼬리
-        cv.line(self.canvas, (26, 10), (26, 11), red)
-        cv.line(self.canvas, (25, 11), (26, 11), red)
-        cv.line(self.canvas, (28, 10), (28, 11), red)
-        cv.line(self.canvas, (29, 11), (28, 11), red)
-        cv.line(self.canvas, (26, 14), (26, 13), red)
-        cv.line(self.canvas, (25, 13), (26, 13), red)
-        cv.line(self.canvas, (29, 14), (29, 13), red)
-        cv.line(self.canvas, (28, 13), (29, 13), red)
+        cv.line(self.canvas, (x+26, y+10), (x+26, y+11), red)
+        cv.line(self.canvas, (x+25, y+11), (x+26, y+11), red)
+        cv.line(self.canvas, (x+28, y+10), (x+28, y+11), red)
+        cv.line(self.canvas, (x+29, y+11), (x+28, y+11), red)
+        cv.line(self.canvas, (x+26, y+14), (x+26, y+13), red)
+        cv.line(self.canvas, (x+25, y+13), (x+26, y+13), red)
+        cv.line(self.canvas, (x+29, y+14), (x+29, y+13), red)
+        cv.line(self.canvas, (x+28, y+13), (x+29, y+13), red)
+    
